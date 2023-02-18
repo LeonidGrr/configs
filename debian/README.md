9405c89a287d026380fea24ad28bf61d9a765136
unknown <leonidgrr@gmail.com>
2023-02-18 13:42:59 +0300

    Removed debian config

--- debian/README.md
+++
@@ -1,8 +0,0 @@
-# Config for Debian Stable
-
-```
-./install.sh (includes nerdfonts.sh and copyconf.sh unless commented)
-sudo reboot
-```
-
-Based on https://github.com/drewgrif/i3gaps-debian
--- debian/copyconf.sh
+++
@@ -1,7 +0,0 @@
-cp -r .config ~;
-chmod +x $HOME/.config/polybar/launch.sh;
-i3-msg restart;
-
-# Git config
-echo 'Cache git credentials for 10 days';
-git config --global credential.helper 'cache --timeout 864000';
